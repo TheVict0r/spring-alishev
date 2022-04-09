6f123c42dd2e9683cea27cf4e3690795c6bf2586
Victor Pravdivets <vpravd1@gmail.com>
2022-04-09 14:21:48 +0300

    06-Dependency Injection - Part 2 - Setter+Getter hardcode value

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -3,6 +3,9 @@ package ru.alishev.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private String volume;
+
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
@@ -14,6 +17,22 @@ public class MusicPlayer {
 
     public void setMusic(Music music) {
         this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
     }
 
     public void playMusic() {
