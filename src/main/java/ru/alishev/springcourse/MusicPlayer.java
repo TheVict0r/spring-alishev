cc0cfcce9d1e74363cc9dbde1bc63c02475ee149
Victor Pravdivets <vpravd1@gmail.com>
2022-04-10 16:22:50 +0300

    11-@Qualifier - Dependency Injection-2

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,40 +1,24 @@
 package ru.alishev.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
-    // можно здесь через рефлексию
-//    @Autowired
-//    private Music Music;
-
-    //можно здесь
-    //@Autowired
-    //public MusicPlayer(Music music) {
-    //    this.Music = music;
-    //}
-
-    //    можно здесь
-    //    @Autowired
-    //    public void setMusic(ru.alishev.springcourse.Music music) {
-    //        Music = music;
-    //    }
-
     public String  playMusic() {
-//        System.out.println("Playing: " + classicalMusic.getSong());
-//        System.out.println("Playing: " + rockMusic.getSong());
-
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
     }
 }
